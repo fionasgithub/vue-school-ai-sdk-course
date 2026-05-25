454d3a14c4165aedc1a32ac52d547e8c8e9d6e68
fiona <sfwang.dev@gmail.com>
2026-05-25 18:01:52 +0800

    feat: ch26 useCompletion

---
+++ nuxt-app/app/pages/generate-article.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useCompletion } from '@ai-sdk/vue'
+
+const { csrf } = useCsrf()
+
+const { completion, isLoading, stop, complete } = useCompletion({
+  api: '/api/completion',
+  headers: {
+    'csrf-token': csrf
+  }
+})
+
+const prompt = ref('Write an article about the benefits of using AI in the workplace.')
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-bold mb-4">
+      Generate Article
+    </h1>
+    <UButton :loading="isLoading" @click="complete(prompt)">
+      Generate Article
+    </UButton>
+    <UButton v-if="isLoading" variant="outline" @click="stop()">
+      Stop
+    </UButton>
+    <UButton v-if="!isLoading" variant="outline" @click="completion = ''">
+      Clear
+    </UButton>
+    <div v-if="completion" class="overflow-y-auto h-full">
+      <ChatComark :markdown="completion" />
+    </div>
+  </div>
+</template>
